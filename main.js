2872c084ebbe0386ba0181a627bb278276b5d8d5
Prameela Pallavi Bellala <pallavibellala123@gmail.com>
2025-04-05 22:27:02 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-console.log("JAVA SCRIPT ALL CONCEPTS PRACTISE");
-// data types 
-let a=10;
-console.log(a)
-
-const b=35;
-b=40;
-console.log(b) // error since const is used so value can't be changed
-
