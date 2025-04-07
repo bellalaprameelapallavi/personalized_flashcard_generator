fbb5db46949a8e167c35fb3190f481862ef51c21
Prameela Pallavi Bellala <pallavibellala123@gmail.com>
2025-04-07 22:42:26 +0530

    Create react-basics new.jsx
    Basics of react

---
+++ react-basics new.jsx
@@ -0,0 +1,6 @@
+/*React - It is a java script library - designed by Facebook.
+It is used to build UI, reusability components, inside js we use html
+JSX - java script X- XML(extensible mark up language)
+components - a piece of UI types ( functional and classic)
+props - read only properties 
+
