be9dee3ae59811c7eaa5de094b859dda0dcd98a2
Prameela Pallavi Bellala <pallavibellala123@gmail.com>
2025-04-07 22:43:09 +0530

    Update react-basics new.jsx

--- react-basics new.jsx
+++ react-basics new.jsx
@@ -3,4 +3,7 @@ It is used to build UI, reusability components, inside js we use html
 JSX - java script X- XML(extensible mark up language)
 components - a piece of UI types ( functional and classic)
 props - read only properties 
+*/
+Example :
+
 
